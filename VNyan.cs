b67f738a64875425a678fae9a8cef2e33f99fffa
Adachi91 <118907877+Adachi91@users.noreply.github.com>
2025-02-06 21:45:39 -0600

    Updated vNyan.
    removed some shit, as you can see.
    I also changed async void to async task as I learned that async void is for legacy support, and I was being dumb, but now I'm a little bit smarter.

--- VNyan.cs
+++ VNyan.cs
@@ -10,50 +10,18 @@ using System.ComponentModel;
 namespace ShimamuraBot
 {
     /// <summary>
-    /// VNyan Websocket Extension to allow chat to tip to interact with VNyan (e.g. Throw items, Bonk, Change Pose, etc... of the VTuber)
-    /// endpoint:: ws://localhost:8000/vnyan
+    ///  VNyan Websocket Extension to allow chat to tip to interact with VNyan (e.g. Throw items, Bonk, Change Pose, etc... of the VTuber)
+    ///  endpoint:: ws://localhost:8000/vnyan
     /// </summary>
-    internal class VNyan
+    internal class VNyan : IDisposable
     {
         private string name = "vNyan";
-        private CancellationTokenSource cts = new CancellationTokenSource();
-        private CancellationToken cancelRequest;
 
-        private ClientWebSocket _ws_connection_port_socket_unix_reverse_uno;
+        public VNyan() { }
+        public void Dispose() { }
 
-        /// <summary>
-        /// Dumstructor.
-        /// </summary>
-        public VNyan() {
-            _ws_connection_port_socket_unix_reverse_uno = new ClientWebSocket();
-            cancelRequest = cts.Token;
-        }
-
-        public void Stop() { //deprecated
-            if (!cts.IsCancellationRequested || !cts.Token.CanBeCanceled || !cts.Token.IsCancellationRequested)
-                cts.Cancel();
-            else
-                Print(this.name, $"A secondary cancellation request was sent to vNyan.", PrintSeverity.Debug);
-        }
-
-        /// <summary>
-        /// Opens the vNyan websocket connection.
-        /// </summary>
-        /// <param name="Host">Optional - Defaults to vNyan default localhost:8000/vnyan</param>
-        /// <returns>Boolean - Connection Status</returns>
-        public async Task<bool> Start(string Host = "ws://127.0.0.1:8000/vnyan") => await _start(Host);
-
-        private async Task<bool> _start(string Host) {
-            try {
-                await _ws_connection_port_socket_unix_reverse_uno.ConnectAsync(new Uri(Host), cancelRequest);
-                if (_ws_connection_port_socket_unix_reverse_uno.State == WebSocketState.Open) return true; else return false;
-            } catch (Exception ex) {
-                new BotException("vNyan", "Could not open a websocket connection.\n", ex);
-                return false;
-            }
-        }
-
-        public void Redeem(string type) { //TODO: add a check to make sure ws connectivity is available for vNyan otherwise return a user friendly message
+        public void Redeem(string type) {
+            /* TODO: Check if socket can be opened before procededing */
             switch(type) {
                 case "yeet":
                     SendTovNyan("Test");
@@ -73,10 +41,13 @@ namespace ShimamuraBot
             }
         }
 
-        private async void SendTovNyan(string msg)
-        {
-            using(ClientWebSocket vNyan = new ClientWebSocket())
-            {
+
+        /// <summary>
+        ///  Send message to vNyan WebSocket
+        /// </summary>
+        /// <param name="msg"><see cref="string"/> Plain string</param>
+        private async Task SendTovNyan(string msg) { // So eh, yeah. I figured out why it crashes sometimes. Don't use `Async Void`s.
+            using (ClientWebSocket vNyan = new ClientWebSocket()) {
                 //holy.
                 //fucking.
                 //shit.
@@ -110,13 +81,10 @@ namespace ShimamuraBot
                 try {
                     //Use IPv4 localhost instead of 'localhost' because it will try and route to IPv6 and bounce around causing up to 1 second latency.
                     await vNyan.ConnectAsync(new Uri("ws://127.0.0.1:8000/vnyan"), default);
-                    _ = vNyan.SendAsync(new ArraySegment<byte>(buffer), WebSocketMessageType.Text, true, default);
+                    await vNyan.SendAsync(buffer, WebSocketMessageType.Text, true, default);
                 } catch (Exception ex) {
-                    new BotException(this.name, $"{ex}");
-                    //Print(this.name, $"[vNyan]: {ex}", PrintSeverity.Error);
+                    new BotException(this.name, $"Error when trying to send vNyan WebSocket message.", ex);
                 }
-
-                //Print($"[vNyan]: Disposing Websocket Client.", 0);
             }
         }
     }
